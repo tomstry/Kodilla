eb5ecfe7cb4abc54c0b677bebe491f97d1d37cb4
Tomasz Stryjak <stryjak.tomasz02@gmail.com>
2022-11-03 22:47:41 +0100

    upgraded code

--- javascript-prework/js/script.js
+++ javascript-prework/js/script.js
@@ -1,10 +1,5 @@
-var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
-
-var buttonRock, buttonPaper, buttonScissors;
-buttonRock = document.getElementById('button-rock');
-
-buttonPaper = document.getElementById('button-paper');
-
+let buttonRock = document.getElementById('button-rock'),
+buttonPaper = document.getElementById('button-paper'),
 buttonScissors = document.getElementById('button-scissors');
 
 
@@ -44,12 +39,11 @@ function buttonClicked(argButtonName) {
         }
         printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
       }
-      playerMove = argButtonName;
-      console.log('wybór ruchu gracza to: ' + playerInput);
+      let playerMove = argButtonName;
       console.log('ruch gracza to: ' + playerMove);
-      randomNumber = Math.floor(Math.random() * 3 + 1);
+      let randomNumber = Math.floor(Math.random() * 3 + 1);
       console.log('wylosowana liczba to: ' + randomNumber);
-      computerMove = getMoveName(randomNumber);
+      let computerMove = getMoveName(randomNumber);
       console.log('ruch komputera to: ' + computerMove);
       displayResult(playerMove, computerMove);
     }

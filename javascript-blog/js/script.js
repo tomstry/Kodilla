6c60dfdb39348e772179f3a60ad05acb6c196ad9
Tomasz Stryjak <stryjak.tomasz02@gmail.com>
2022-11-09 10:12:19 +0100

    titleclickhandler almost done

--- javascript-blog/js/script.js
+++ javascript-blog/js/script.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function titleClickHandler(event){
-    console.log('Link was clicked!');
+    event.preventDefault();
+    const clickedElement = this;
+    console.log('clickedElement (with plus): ' + clickedElement);
     console.log(event)
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -9,14 +11,13 @@ function titleClickHandler(event){
         activeLink.classList.remove('active');
     }
 
+    /* add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
-  
     /* get 'href' attribute from the clicked link */
   
     /* find the correct article using the selector (value of 'href' attribute) */

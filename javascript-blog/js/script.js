f6c77933e2f23961fcb9845bd42116c69d7e075a
Tomasz Stryjak <stryjak.tomasz02@gmail.com>
2022-11-09 16:39:52 +0100

    display article after click

--- javascript-blog/js/script.js
+++ javascript-blog/js/script.js
@@ -3,14 +3,12 @@
 function titleClickHandler(event){
     event.preventDefault();
     const clickedElement = this;
-    console.log('clickedElement (with plus): ' + clickedElement);
-    console.log(event)
+    console.log('link was clicked!')
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-
     /* add class 'active' to the clicked link */
     clickedElement.classList.add('active');
     /* remove class 'active' from all articles */
@@ -19,10 +17,13 @@ function titleClickHandler(event){
         activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
-  
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
